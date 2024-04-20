e333b662144cf8733de8703bcd2b6770eb88288f
phungkhaccuong <cuongst93@gmail.com>
2024-04-20 22:23:37 +0700

    u

--- openkaito/search/ranking/__init__.py
+++ openkaito/search/ranking/__init__.py
@@ -2,7 +2,3 @@ from .abstract_model import AbstractRankingModel
 from .heuristic_ranking import HeuristicRankingModel
 from .trivial_ranking import TrivialRankingModel
 from .optimize_ranking import OptimizeRankingModel, OptimizeRankingModelV1
-
-
-class OptimizeRankingModel:
-    pass
