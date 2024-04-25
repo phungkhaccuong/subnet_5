444115a2ee4474f79d4ba526e53dbfc9622d0f21
phungkhaccuong <cuongst93@gmail.com>
2024-04-25 23:13:05 +0700

    get choice and reason in doc

--- scripts/author_index_evaluation.py
+++ scripts/author_index_evaluation.py
@@ -1,25 +1,20 @@
-import argparse
 import os
-import random
 
-import bittensor as bt
 import openai
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
 
 from openkaito.crawlers.twitter.apidojo import ApiDojoTwitterCrawler
-from openkaito.crawlers.twitter.microworlds import MicroworldsTwitterCrawler
 from openkaito.evaluation.evaluator import Evaluator
-from openkaito.protocol import SearchSynapse, SortType, StructuredSearchSynapse
-from openkaito.search.ranking.heuristic_ranking import HeuristicRankingModel
+from openkaito.search.ranking import OptimizeRankingModel, OptimizeRankingModelV1, HeuristicRankingModel
 from openkaito.search.structured_search_engine import StructuredSearchEngine
 from openkaito.tasks import generate_author_index_task
 
 
 def main():
     load_dotenv()
-    bt.logging.set_debug(True)
-    bt.logging.set_trace(True)
+    # bt.logging.set_debug(True)
+    # bt.logging.set_trace(True)
 
     # for ranking results evaluation
     llm_client = openai.OpenAI(
@@ -30,7 +25,7 @@ def main():
 
     twitter_crawler = None
     # # for integrity check
-    # twitter_crawler = ApiDojoTwitterCrawler(os.environ["APIFY_API_KEY"])
+    twitter_crawler = ApiDojoTwitterCrawler(os.environ["APIFY_API_KEY"])
     evaluator = Evaluator(llm_client, twitter_crawler)
 
     search_client = Elasticsearch(
@@ -43,28 +38,39 @@ def main():
         ssl_show_warn=False,
     )
 
-    search_engine = StructuredSearchEngine(
-        search_client=search_client,
-        relevance_ranking_model=HeuristicRankingModel(
-            length_weight=0.8, age_weight=0.2
-        ),
-        twitter_crawler=None,
-    )
+    ranking_models = [
+        HeuristicRankingModel(length_weight=0.8, age_weight=0.2),
+        OptimizeRankingModel(length_weight=0.8, age_weight=0.2),
+        OptimizeRankingModelV1(length_weight=0.77, age_weight=0.23),
+    ]
+    search_engines = [
+        StructuredSearchEngine(
+            search_client=search_client,
+            relevance_ranking_model=model,
+            twitter_crawler=None,
+            recall_size=100
+        )
 
-    # search_query = StructuredSearchSynapse(
-    #     size=10, author_usernames=["elonmusk", "nftbadger"]
-    # )
-    search_query = generate_author_index_task(size=10, num_authors=100)
+        for model in ranking_models
+    ]
+
+    search_query = generate_author_index_task(size=10, num_authors=2)
     print(search_query)
 
-    docs = search_engine.search(search_query=search_query)
-    print("======documents======")
-    print(docs)
+    responses = [search_engine.search(search_query=search_query) for search_engine in search_engines]
 
-    score = evaluator.llm_author_index_data_evaluation(docs)
-    print("======LLM Score======")
-    print(score)
+    rewards = evaluator.evaluate(search_query, responses)
+    print(f"Scored responses: {rewards}")
+    return rewards
 
 
 if __name__ == "__main__":
-    main()
+    optimize = 0
+    default = 0
+    for i in range(0, 50):
+        result = main()
+        if result[1] >= result[0] and result[1] >= result[2]:
+            optimize += 1
+        else:
+            default += 1
+        print(f"optimize::{optimize} default::{default}")
