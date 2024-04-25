8ed66bcfe9d07ede2e5001fcc7ca71138f72ca54
phungkhaccuong <cuongst93@gmail.com>
2024-04-25 23:31:04 +0700

    index model

--- openkaito/search/ranking/__init__.py
+++ openkaito/search/ranking/__init__.py
@@ -1,4 +1,4 @@
 from .abstract_model import AbstractRankingModel
 from .heuristic_ranking import HeuristicRankingModel
 from .trivial_ranking import TrivialRankingModel
-from  .optimize_ranking import OptimizeRankingModel
+from  .optimize_ranking import OptimizeRankingModel, OptimizeRankingModelV1
