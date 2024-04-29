0f1724e8c698d5b069c47c8c6f32274e0390ffbc
phungkhaccuong <cuongst93@gmail.com>
2024-04-29 10:22:12 +0700

    u

--- scripts/gen_question.py
+++ scripts/gen_question.py
@@ -6,7 +6,6 @@ import os
 
 def gen_question():
     root_dir = __file__.split("scripts")[0]
-    print(root_dir)
     dataset_dir = root_dir + "datasets/eth_denver_dataset"
     eth_denver_dataset_dir = dataset_dir
 
@@ -16,19 +15,17 @@ def gen_question():
         max_retries=3,
     )
 
-    print(f"eth_denver_dataset_dir::::{eth_denver_dataset_dir}")
+    segments = random_eth_denver_segments(
+        eth_denver_dataset_dir, num_sources=3
+    )
 
-    # segments = random_eth_denver_segments(
-    #     eth_denver_dataset_dir, num_sources=3
-    # )
+    print(f'segment::::::::::::::{segments}')
 
-    # print(f'segment::::::::::::::{segments}')
-    #
-    # question = generate_question_from_eth_denver_segments(
-    #     llm_client, segments
-    # )
-    #
-    # print(f'question:::::::::::::::{question}')
+    question = generate_question_from_eth_denver_segments(
+        llm_client, segments
+    )
+
+    print(f'question:::::::::::::::{question}')
 
 if __name__ == '__main__':
     gen_question()
