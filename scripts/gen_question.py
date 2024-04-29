669097d9eedc827d8b503b467bcf48cd2daa547c
phungkhaccuong <cuongst93@gmail.com>
2024-04-29 09:54:09 +0700

    u

---
+++ scripts/gen_question.py
@@ -0,0 +1,31 @@
+from openkaito.tasks import random_eth_denver_segments, generate_question_from_eth_denver_segments
+
+import openai
+import os
+
+
+def gen_question():
+    root_dir = __file__.split("neurons")[0]
+    dataset_dir = root_dir + "datasets/eth_denver_dataset"
+    eth_denver_dataset_dir = dataset_dir
+
+    llm_client = openai.OpenAI(
+        api_key=os.environ["OPENAI_API_KEY"],
+        organization=os.getenv("OPENAI_ORGANIZATION"),
+        max_retries=3,
+    )
+
+    segments = random_eth_denver_segments(
+        eth_denver_dataset_dir, num_sources=3
+    )
+
+    print(f'segment::::::::::::::{segments}')
+
+    question = generate_question_from_eth_denver_segments(
+        llm_client, segments
+    )
+
+    print(f'question:::::::::::::::{question}')
+
+if __name__ == '__main__':
+    gen_question()
