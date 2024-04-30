1910d16eb18f5c37f4e7cb7ebfe6a54f3055eaab
phungkhaccuong <cuongst93@gmail.com>
2024-05-01 00:24:57 +0700

    u

--- scripts/vector_search_testing.py
+++ scripts/vector_search_testing.py
@@ -261,7 +261,7 @@ if __name__ == "__main__":
     # search(search_client)
 
     #Example query
-    query_text = "What are the benefits of Bernhard Borges' decentralized serverless compute solution?"
+    query_text = "What is the architecture of Cloudless Functions in Fluence?"
     embedding = text_embedding(query_text)[0]
     embedding = pad_tensor(embedding, max_len=MAX_EMBEDDING_DIM)
 
