f0002f3fe7415ed4944c208cb65c133a47476553
phungkhaccuong <cuongst93@gmail.com>
2024-05-02 23:05:56 +0700

    u

---
+++ scripts/get_speaker_dict.py
@@ -0,0 +1,43 @@
+
+from tqdm import tqdm
+import json
+
+root_dir = __file__.split("scripts")[0]
+index_name = "eth_denver_v1"
+
+def get_speaker_dict():
+    """Index documents in Elasticsearch"""
+
+    dataset_dir = root_dir + "datasets/eth_denver_dataset"
+    dataset_path = Path(dataset_dir)
+
+    num_files = len(list(dataset_path.glob("*.json")))
+    print(f"Indexing {num_files} files in {dataset_dir}")
+
+    speaker_dict = {}
+    for doc_file in tqdm(
+            dataset_path.glob("*.json"), total=num_files, desc="Indexing docs"
+    ):
+        with open(doc_file, "r") as f:
+            doc = json.load(f)
+            speaker = doc['speaker']
+            episode_id = doc['episode_id']
+            if speaker in speaker_dict:
+                speaker_dict[speaker].append(episode_id)
+            else:
+                speaker_dict[speaker] = [episode_id]
+
+    # Save speaker_episode_dict to a JSON file
+    with open('speaker_dict.json', 'w') as json_file:
+        json.dump(speaker_dict, json_file)
+
+    print("JSON file has been created successfully!")
+    print(speaker_dict)
+
+
+
+if __name__ == "__main__":
+    get_speaker_dict()
+
+
+
